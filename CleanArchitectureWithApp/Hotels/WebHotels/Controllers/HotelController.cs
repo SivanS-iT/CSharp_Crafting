98655ccae3f8c06443b0bb59fd4880ef39913338
Ivan Sambol <ivan.sambol@br-automation.com>
2024-05-10 01:35:00 +0200

    CRUD operation for Hotel, notification on change

--- CleanArchitectureWithApp/Hotels/WebHotels/Controllers/HotelController.cs
+++ CleanArchitectureWithApp/Hotels/WebHotels/Controllers/HotelController.cs
@@ -36,6 +36,8 @@ namespace WebHotels.Web.Controllers
             {
                 _db.Hotels.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The hotel has been added successfully.";
+
                 return RedirectToAction("Index");
             }
             return View();
@@ -51,5 +53,53 @@ namespace WebHotels.Web.Controllers
             }
             return View(obj);
         }
+
+        [HttpPost]
+        public IActionResult Update(Hotel obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Hotels.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The hotel has been updated successfully.";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        /// <summary>
+        /// Delete get endpoint
+        /// </summary>
+        /// <param name="hotelId"></param>
+        /// <returns></returns>
+        public IActionResult Delete(int hotelId)
+        {
+            Hotel? obj = _db.Hotels.FirstOrDefault(u => u.Id == hotelId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Hotel obj)
+        {
+            Hotel? objForDelete = _db.Hotels.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objForDelete is not null)
+            {
+                _db.Hotels.Remove(objForDelete);
+                _db.SaveChanges();
+                TempData["success"] = "The hotel has been deleted successfully.";
+
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Hotel could not be deleted";
+            return View();
+        }
+
     }
 }
