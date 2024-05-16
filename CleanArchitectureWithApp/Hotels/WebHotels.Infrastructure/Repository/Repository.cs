6f3dee0e2885504b1b53e7fd7dc24ae4d99d7da2
Ivan Sambol <ivan.sambol1999@gmail.com>
2024-05-16 09:43:24 +0200

    Implement structure for generic IRepository

---
+++ CleanArchitectureWithApp/Hotels/WebHotels.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WebHotels.Application.Common.Interfaces;
+
+namespace WebHotels.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null, bool tracked = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
