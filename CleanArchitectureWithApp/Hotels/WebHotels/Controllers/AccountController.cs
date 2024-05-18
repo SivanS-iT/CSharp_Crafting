155f5375fed2b5314aa8dc31dd79a9c5515a3755
Ivan Sambol <ivan.sambol@br-automation.com>
2024-05-18 17:43:25 +0200

    Add view for register endpoint and controller action

--- CleanArchitectureWithApp/Hotels/WebHotels/Controllers/AccountController.cs
+++ CleanArchitectureWithApp/Hotels/WebHotels/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WebHotels.Domain.Entities;
 using WebHotels.Web.ViewModels;
 
@@ -36,9 +37,26 @@ namespace WebHotels.Web.Controllers
             return View(loginVM);
         }
 
-        public IActionResult Register()
+        public IActionResult Register(string returnUrl = null)
         {
-            return View();
+            returnUrl ??= Url.Content("~/");
+            if (!_roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+                _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+            }
+
+            RegisterVM registerVM = new()
+            {
+                RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                }),
+                RedirectUrl = returnUrl
+            };
+
+            return View(registerVM);
         }
     }
 }
