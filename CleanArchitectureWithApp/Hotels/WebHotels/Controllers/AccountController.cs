ad3ad64a9eea42eabb10e8a21b42e251e1ad2d4c
Ivan Sambol <ivan.sambol@br-automation.com>
2024-05-18 16:43:30 +0200

    Add login and registor button to nav and AccountController

---
+++ CleanArchitectureWithApp/Hotels/WebHotels/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebHotels.Domain.Entities;
+using WebHotels.Web.ViewModels;
+
+namespace WebHotels.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+
+        public IActionResult Login(string returnUrl = null)
+        {
+
+            returnUrl ??= Url.Content("~/");
+
+            LoginVM loginVM = new()
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
