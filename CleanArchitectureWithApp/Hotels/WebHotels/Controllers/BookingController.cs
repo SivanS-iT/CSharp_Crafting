75030b5966c5456997e16837004040edb0fca5d2
Ivan Sambol <ivan.sambol@br-automation.com>
2024-05-20 15:25:09 +0200

    Create Booking Controller

---
+++ CleanArchitectureWithApp/Hotels/WebHotels/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebHotels.Application.Common.Interfaces;
+using WebHotels.Domain.Entities;
+
+namespace WebHotels.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BookingController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult FinalizeBooking(int hotelId, DateOnly checkInDate, int nights)
+        {
+            Booking booking = new()
+            {
+                HotelId = hotelId,
+                Hotel = _unitOfWork.Hotel.Get(u => u.Id == hotelId, includeProperties: "HotelAmenity"),
+                CheckInDate = checkInDate,
+                Nights = nights,
+                CheckOutDate = checkInDate.AddDays(nights),
+            };
+
+            return View(booking);
+        }
+    }
+}
--- CleanArchitectureWithApp/Hotels/WebHotels/Controllers/HomeController.cs
+++ CleanArchitectureWithApp/Hotels/WebHotels/Controllers/HomeController.cs
@@ -26,20 +26,6 @@ namespace WebHotels.Controllers
             return View(homeVM);
         }
 
-        [HttpPost]
-        public IActionResult Index(HomeVM homeVM)
-        {
-            homeVM.HotelList = _unitOfWork.Hotel.GetAll(includeProperties: "HotelAmenity");
-            foreach (var hotel in homeVM.HotelList)
-            {
-                if (hotel.Id % 2 == 0)
-                {
-                    hotel.IsAvailable = false;
-                }
-            }
-            return View(homeVM);
-        }
-
 
         public IActionResult GetHotelsByDate(int nights, DateOnly checkInDate)
         {
