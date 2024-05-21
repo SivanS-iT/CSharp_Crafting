3abdd192894fa27e610b39a6683c45921ab59c16
Ivan Sambol <ivan.sambol@br-automation.com>
2024-05-21 14:42:10 +0200

    Add methodes to BookingRepository

--- CleanArchitectureWithApp/Hotels/WebHotels.Application/Common/Interfaces/IBookingRepository.cs
+++ CleanArchitectureWithApp/Hotels/WebHotels.Application/Common/Interfaces/IBookingRepository.cs
@@ -11,5 +11,7 @@ namespace WebHotels.Application.Common.Interfaces
     public interface IBookingRepository : IRepository<Booking>
     {
         void Update(Booking entity);
+        void UpdateStatus(int bookingId, string bookingStatus, int villaNumber);
+        void UpdateStripePaymentID(int bookingId, string sessionId, string paymentIntentId);
     }
 }
