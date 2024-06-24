038d605bf22cb34381275454dc297d56dc212756
Ivan Sambol <ivan.sambol@br-automation.com>
2024-06-24 09:55:05 +0200

    Add Employee entity and interface for it

---
+++ BlazorCleanArchitecture/Application/Contracts/IEmpoyee.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts
+{
+    public interface IEmpoyee
+    {
+        Task<ServiceResponse> AddAsync(Employee employee);
+        Task<ServiceResponse> UpdateAsync(Employee employee);
+        Task<ServiceResponse> DeleteAsync(int id);
+        Task<ServiceResponse> GetAsync();
+        Task<ServiceResponse> GetByIdAsync(int id);
+
+    }
+}
