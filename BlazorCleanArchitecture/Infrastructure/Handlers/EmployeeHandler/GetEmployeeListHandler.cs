75a39b416bd622ae87975f79cf7c2675facc4e1b
Ivan Sambol <ivan.sambol@br-automation.com>
2024-07-05 23:45:02 +0200

    Create structure for MediatR

---
+++ BlazorCleanArchitecture/Infrastructure/Handlers/EmployeeHandler/GetEmployeeListHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Queries.EmployeeQuery;
+using Domain.Entities;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Handlers.EmployeeHandler
+{
+    public class GetEmployeeListHandler : IRequestHandler<GetEmployeeListQuery, List<Employee>>
+    {
+        private readonly AppDbContext _appDbContext;
+        public GetEmployeeListHandler(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext; 
+        }
+        public async Task<List<Employee>> Handle(GetEmployeeListQuery request, CancellationToken cancellationToken)
+        {
+            return await _appDbContext.Employees.AsNoTracking().ToListAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
--- BlazorCleanArchitecture/WebAssembly/Program.cs
+++ BlazorCleanArchitecture/WebAssembly/Program.cs
@@ -1,5 +1,7 @@
+using Application.Queries.EmployeeQuery;
 using Application.Services;
 using Blazored.Toast;
+using Infrastructure.Handlers.EmployeeHandler;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebAssembly;
@@ -11,5 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7101/") });
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddBlazoredToast();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetEmployeeListHandler).Assembly));
+
 
 await builder.Build().RunAsync();
