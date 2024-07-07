36c03ae0f96526e26bfaebb9d09aa443a9723a30
Ivan Sambol <ivan.sambol@br-automation.com>
2024-07-07 19:05:47 +0200

    Add custom Migration to delete column RoleId

---
+++ BlazorCleanArchitecture/Infrastructure/Migrations/20240707165436_CleanUpCollumRoleId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanUpCollumRoleId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
