dd8813e99c468663bc8a055959228ef8dc8f9307
Ivan Sambol <ivan.sambol@br-automation.com>
2024-07-11 15:10:12 +0200

    Add xUnit test solution for UnitTests

---
+++ BlazorCleanArchitecture/Application.UnitTests/Employee/Commands/CreateEmployeeCommandHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Application.Commands;
+using Domain.Features.Employee;
+using Moq;
+
+namespace Application.UnitTests.Employee.Commands
+{
+    //private readonly Mock<IEmployeeRepository> _employeeRpositoryMock;
+
+    public class CreateEmployeeCommandHandlerTests
+    {
+        [Fact]
+        public void Handle_Should_ReturnHailureResult_WhenEmployeeNameIsNotUnique()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
