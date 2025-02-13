f4fb307e532f3b5dded5529be6b1d4f841830032
ivansambol <ivan.sambol1999@gmail.com>
2025-02-13 13:37:52 +0100

    Make Commands sealed

--- BlazorCleanArchitecture/Domain/Features/Employee/CreateEmployeeRequest.cs
+++ BlazorCleanArchitecture/Domain/Features/Employee/CreateEmployeeRequest.cs
@@ -10,7 +10,6 @@ namespace Domain.Features.Employee
     {
         public string Name { get; set; }
         public string? Address { get; set; }
-        
         public string Email { get; set; }
     }
 }
